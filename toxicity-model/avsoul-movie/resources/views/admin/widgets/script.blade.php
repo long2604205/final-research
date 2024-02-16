0cf9c9eb1d8c395371c59b77e3a7a764014bd4b4
杨志龙 <zhilong2412@gmail.com>
2024-02-16 15:15:42 +0700

    second-comit
    remove project trash

--- toxicity-model/avsoul-movie/resources/views/admin/widgets/script.blade.php
+++
@@ -1,7 +0,0 @@
-<script src="{{asset('admin/js/jquery-3.5.1.min.js')}}"></script>
-<script src="{{asset('admin/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('admin/js/jquery.magnific-popup.min.js')}}"></script>
-<script src="{{asset('admin/js/smooth-scrollbar.js')}}"></script>
-<script src="{{asset('admin/js/select2.min.js')}}"></script>
-{{-- <script src="{{asset('admin/js/admin.js')}}"></script> --}}
-<script src="{{asset('admin/js/admin.js')}}"></script>
